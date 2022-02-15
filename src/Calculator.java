d1a9f4eebe0bd3829cce4863e9448ec100160713
Rashid <rashidiscool21@gmail.com>
2022-02-15 10:27:59 +0000

    F

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,55 +1,57 @@
 public class Calculator {
+
+    // Declaring userName and runningTotal variables.
     private final String userName;
     private double runningTotal;
 
-
+    // Method for getUserName **Not sure what this does**
     public Calculator(String getUserName){
         userName = getUserName;
 
     }
-
+    // Method for outputting the current running total of all the calculations up to that point.
     public double getRunningTotal(){
         return runningTotal;
     }
 
+    //Method for adding two numbers (num1,num2) together then adding and replacing the running total with the "sum".
     public double addTwoNumbers(double num1, double num2) {
         double sum = num1 + num2;
         runningTotal += sum;
         return sum;
-
-
     }
+
+    //Method for subtracting two numbers (num1,num2) together then adding and replacing the running total with the "sum"
     public double subtractTwoNumbers(double num1, double num2) {
         double sum = num1 - num2;
         runningTotal += sum;
         return sum;
-
     }
+
+    //Method for multiplying two numbers (num1,num2) together then adding and replacing the running total with the "sum"
     public double multiplyTwoNumbers(double num1, double num2) {
         double sum = num1 * num2;
         runningTotal += sum;
         return sum;
     }
+
+    //Method for dividing two numbers (num1,num2) together then adding and replacing the running total with the "sum".
     public double divideTwoNumbers(double num1, double num2) {
         double sum = num1 / num2;
         runningTotal += sum;
         return sum;
-
-
     }
+
+    /* Method using boolean to check if a number (checkNum) is even or odd (true or false) by using mod to see if the
+     number is divisible by 2 therefore is even.
+     */
     public boolean checkIsEven(int checkNum) {
         return checkNum % 2 == 0;
-
     }
+
+    // Method that takes current users name and displays their running total with it.
     public String getCurrentStatus() {
         return userName + "'s running total is " + runningTotal;
-
-
-
     }
-
-
-
-
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
+
+        // Calling instance of the Calculator Class
+        Calculator calculator = new Calculator("Rashid");
+
+        // Generating two random numbers
         double num1 = Math.random();
         double num2 = Math.random();
 
-        Calculator calculator = new Calculator("Rashid");
+        // Using method "addTwoNumbers" to add two random numbers
         double add = calculator.addTwoNumbers(num1 , num2);
+
+        // Output for "addTwoNumbers" method
         System.out.println("Add Two Random Numbers");
         System.out.println(num1);
         System.out.println("+");
@@ -14,12 +21,14 @@ public class Main {
         System.out.println(add);
         System.out.println();
 
-
+        // Generating two "new" random numbers
         num1 = Math.random();
         num2 = Math.random();
 
-
+        // Using method "subtractTwoNumbers" to subtract two random numbers.
         double sub = calculator.subtractTwoNumbers(num1 , num2);
+
+        // Output for "subtractTwoNumbers" method
         System.out.println("Subtract Two Random Numbers");
         System.out.println(num1);
         System.out.println("-");
@@ -28,12 +37,14 @@ public class Main {
         System.out.println(sub);
         System.out.println();
 
-
+        // Generating two "new" random numbers
         num1 = Math.random();
         num2 = Math.random();
 
-
+        // Using method "multiplyTwoNumbers" to multiply two random numbers.
         double multi = calculator.multiplyTwoNumbers(num1 , num2);
+
+        // Output for "multiplyTwoNumbers" method
         System.out.println("Multiply Two Random Numbers");
         System.out.println(num1);
         System.out.println("*");
@@ -42,11 +53,14 @@ public class Main {
         System.out.println(multi);
         System.out.println();
 
+        // Generating two "new" random numbers
         num1 = Math.random();
         num2 = Math.random();
 
-
+        // Using method "divideTwoNumbers" to divide two random numbers.
         double divide = calculator.divideTwoNumbers(num1 , num2);
+
+        // Output for "divideTwoNumbers" method
         System.out.println("Divide Two Random Numbers");
         System.out.println(num1);
         System.out.println("/");
@@ -55,29 +69,23 @@ public class Main {
         System.out.println(divide);
         System.out.println();
 
+        // Declaring new variable intTotal to equal runningTotal multiplied by 10 to get an integer.
         int intTotal = (int) (calculator.getRunningTotal() * 10);
 
+        // Running Total output
         System.out.println("Running Total:");
 
+        // Using method "getCurrentStatus" to output the current user's running total.
         String currentStatus = calculator.getCurrentStatus();
         System.out.println(currentStatus);
 
+       // Using method "checkIsEven" to check if the "intTotal" sum is even or odd then outputting the running total.
         boolean evenOrOdd = calculator.checkIsEven(intTotal);
         if(evenOrOdd) {
             System.out.println(calculator.getRunningTotal() + " is even");
         } else {
             System.out.println(calculator.getRunningTotal() + " is odd");
         }
-
-
-
-
-
-
-
-
-
-
 
     }
 
